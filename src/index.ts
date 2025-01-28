92514af585400aa24b3cbde9053c89cac2bfbc75
Steve Macfarlane <stevemacfarlane@MacBook-Air.modem>
2025-01-29 07:58:43 +1000

    update exports and add gitignore

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,5 @@ export * from './context/NotesVisibilityContext';
 export { default as NotesToggle } from './components/NotesToggle';
 export { default as DashboardTile } from './components/DashboardTile';
 export { default as InfoAlert } from './components/InfoAlert';
+export {useNotesVisibility} from './context/NotesVisibilityContext';
+export {usePreserveDeveloperQuery} from './utils/withDeveloperQuery';
