7aae2a3c3c4c4178639cb5510cc51dffadcd45ad
Steve Macfarlane <stevemacfarlane@MacBook-Air.modem>
2025-01-29 08:32:03 +1000

    remove dashboard tile

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
 export * from './helpers/clsxMerge';
-export * from './components/DashboardTile';
 export * from './components/InfoAlert';
 export * from './context/NotesVisibilityContext';
 export { default as NotesToggle } from './components/NotesToggle';
-export { default as DashboardTile } from './components/DashboardTile';
 export { default as InfoAlert } from './components/InfoAlert';
 export {useNotesVisibility} from './context/NotesVisibilityContext';
 export {usePreserveDeveloperQuery} from './utils/withDeveloperQuery';
