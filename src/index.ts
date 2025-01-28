ccd432dd7d52eaa3b26151f840dcdda45b913e47
Steve Macfarlane <stevemacfarlane@MacBook-Air.modem>
2025-01-28 15:27:02 +1000

    add additional components

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,5 @@ export * from './components/DashboardTile';
 export * from './components/InfoAlert';
 export * from './context/NotesVisibilityContext';
 export { default as NotesToggle } from './components/NotesToggle';
+export { default as DashboardTile } from './components/DashboardTile';
+export { default as InfoAlert } from './components/InfoAlert';
